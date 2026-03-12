ca1b3741b8957290ca054847bd999bef403a7b5a
hanfeiyu <53329988+hanfeiyu@users.noreply.github.com>
2026-03-12 04:19:09 +0000

    Deploying to gh-pages from @ hanfeiyu/hanfeiyu.github.io@e9ab31aad7850d8e81fac2231be0a8709b0de142 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "About",
+    id: "nav-home",
+    title: "Home",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
